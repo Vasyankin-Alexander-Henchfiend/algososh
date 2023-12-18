1daee16743a992d6780ff6730559cb23ff838989
Александр Васянькин <henchfiend@yandex.ru>
2023-12-18 20:49:27 +0300

    Первые шаги

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./string.module.css";
 
 export const StringComponent: React.FC = () => {
+
+  const [ inputValue, setInputValue ] = useState<string>()
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.container}>
+        <Input type="text" isLimitText={true} maxLength={11} value={inputValue} onChange={onChange}></Input>
+        <Button text='Развернуть' type='button' isLoader={false}/>
+      </div>
     </SolutionLayout>
   );
 };
